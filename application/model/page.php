51298ca83c29de1d72d3c6acc56a708e1c143fbb
james <james@james.com>
2012-11-29 17:00:56 +1100

    page

--- application/model/page.php
+++ application/model/page.php
@@ -1,29 +1,32 @@
 <?php
+
 namespace App\Model;
 
 use \App\Model\Base\Page as Base_Page;
 use \Zx\Model\Mysql;
 
-class Page extends Base_Page{
+class Page extends Base_Page {
+
     /**
      *
      * @param intval $cat_id  category id
      * @return boolean
      */
-    public static function exist_page_under_cat($cat_id)
-    {
+    public static function exist_page_under_cat($cat_id) {
         $where = 'cat_id=' . $cat_id;
         $num = parent::get_num($where);
-        if ($num>0) return true;
-        else return false;
+        if ($num > 0)
+            return true;
+        else
+            return false;
     }
+
     /**
      * 
      * @param int $id
      * @return string or boolean when false
      */
-    public static function get_title_by_id($id)
-    {
+    public static function get_title_by_id($id) {
         $page = parent::get_one($id);
         if ($page) {
             return $page['title'];
@@ -31,34 +34,44 @@ class Page extends Base_Page{
             return false;
         }
     }
+
     /**
      * only one record or false
      * @return 1D array or false
      */
-    public static function get_about_us()
-    {
+    public static function get_about_us() {
         $where = "ac.title='about us'";
         return parent::get_one_by_where($where);
     }
+
     /**
      * only one record or false
      * @return 1D array or false
      */
-    public static function get_term_condition()
-    {
+    public static function get_terms() {
         $where = "ac.title='term & condition'";
-        return parent::get_one_by_where($where);	
+        return parent::get_one_by_where($where);
     }
+
+    /**
+     * only one record or false
+     * @return 1D array or false
+     */
+    public static function get_privacy() {
+        $where = "ac.title='privacy'";
+        return parent::get_one_by_where($where);
+    }
+
     /**
      * multiple record or false
      * @return 2D array or false
      */
-    public static function get_faqs()
-    {
+    public static function get_faqs() {
         $where = "ac.title='faq'";
-        return parent::get_all($where);		
+        return parent::get_all($where);
     }
-/**
+
+    /**
      * get active cats order by category name
      */
     public static function get_active_pages_by_page_num($page_num = 1, $order_by = 'a.display_order', $direction = 'ASC') {
@@ -66,19 +79,23 @@ class Page extends Base_Page{
         $offset = ($page_num - 1) * NUM_OF_ARTICLES_IN_CAT_PAGE;
         return parent::get_all($where, $offset, NUM_OF_ARTICLES_IN_CAT_PAGE, $order_by, $direction);
     }
+
     public static function get_num_of_active_pages() {
-		$where = ' status=1';
+        $where = ' status=1';
         return parent::get_num();
     }
+
     public static function get_active_pages_by_cat_id_and_page_num($cat_id, $page_num = 1, $order_by = 'a.display_order', $direction = 'ASC') {
         $where = ' a.status=1 AND a.cat_id=' . $cat_id;
         $offset = ($page_num - 1) * NUM_OF_ARTICLES_IN_CAT_PAGE;
         return parent::get_all($where, $offset, NUM_OF_ARTICLES_IN_CAT_PAGE, $order_by, $direction);
     }
+
     public static function get_num_of_active_pages_by_cat_id() {
-		$where = ' a.status=1 AND a.cat_id=' . $cat_id;
+        $where = ' a.status=1 AND a.cat_id=' . $cat_id;
         return parent::get_num();
     }
+
     public static function get_pages_by_page_num($page_num = 1, $order_by = 'id', $direction = 'ASC') {
         $page_num = intval($page_num);
         $page_num = ($page_num > 0) ? $page_num : 1;
@@ -107,5 +124,6 @@ class Page extends Base_Page{
      */
     public static function get_all_pages() {
         return parent::get_all();
-    }	
+    }
+
 }
